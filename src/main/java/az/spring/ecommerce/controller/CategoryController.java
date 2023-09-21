8d7e45678e285e9081a5b16fd29369fec3cfb6cd
Orkhan93 <dokta228@mail.ru>
2023-09-21 21:48:09 +0400

    Added optimization works in CategoryController and CategoryService parts

--- src/main/java/az/spring/ecommerce/controller/CategoryController.java
+++ src/main/java/az/spring/ecommerce/controller/CategoryController.java
@@ -1,12 +1,9 @@
 package az.spring.ecommerce.controller;
 
-import az.spring.ecommerce.constant.CommerceConstant;
 import az.spring.ecommerce.model.Category;
 import az.spring.ecommerce.request.CategoryRequest;
 import az.spring.ecommerce.service.CategoryService;
-import az.spring.ecommerce.utils.CommerceUtil;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -27,32 +23,17 @@ public class CategoryController {
 
     @PostMapping("/add")
     public ResponseEntity<String> addCategory(@RequestBody CategoryRequest categoryRequest) {
-        try {
             return categoryService.addCategory(categoryRequest);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-        return CommerceUtil.getResponseMessage(CommerceConstant.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @GetMapping("/get")
     public ResponseEntity<List<Category>> getAllCategory(@RequestParam(required = false) String filterValue) {
-        try {
             return categoryService.getAllCategory(filterValue);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @PostMapping("/update")
     public ResponseEntity<String> updateCategory(@RequestBody CategoryRequest categoryRequest) {
-        try {
             return categoryService.updateCategory(categoryRequest);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-        return CommerceUtil.getResponseMessage(CommerceConstant.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 }
