b637d4ef566f1027d6901aaff533dd9c8cfd0ab3
Orkhan93 <dokta228@mail.ru>
2023-09-22 23:13:42 +0400

    Optimization works in ProductController and ProductService parts

--- src/main/java/az/spring/ecommerce/controller/ProductController.java
+++ src/main/java/az/spring/ecommerce/controller/ProductController.java
@@ -1,14 +1,9 @@
 package az.spring.ecommerce.controller;
 
-import az.spring.ecommerce.constant.CommerceConstant;
-import az.spring.ecommerce.mappers.ProductMapper;
-import az.spring.ecommerce.model.Product;
 import az.spring.ecommerce.request.ProductRequest;
 import az.spring.ecommerce.service.ProductService;
-import az.spring.ecommerce.utils.CommerceUtil;
 import az.spring.ecommerce.wrapper.ProductWrapper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -30,72 +24,37 @@ public class ProductController {
 
     @PostMapping("/add")
     public ResponseEntity<String> addProduct(@RequestBody ProductRequest productRequest) {
-        try {
             return productService.addProduct(productRequest);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-        return CommerceUtil.getResponseMessage(CommerceConstant.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @GetMapping("get")
     public ResponseEntity<List<ProductWrapper>> getAllProduct() {
-        try {
             return productService.getAllProduct();
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @PostMapping("update")
     public ResponseEntity<String> updateProduct(@RequestBody ProductRequest productRequest) {
-        try {
             return productService.updateProduct(productRequest);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-        return CommerceUtil.getResponseMessage(CommerceConstant.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<String> deleteProduct(@PathVariable Long id) {
-        try {
             return productService.deleteProduct(id);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-        return CommerceUtil.getResponseMessage(CommerceConstant.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @PostMapping("updateStatus")
     public ResponseEntity<String> updateStatus(@RequestBody ProductRequest productRequest) {
-        try {
             return productService.updateStatus(productRequest);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-        return CommerceUtil.getResponseMessage(CommerceConstant.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @GetMapping("/getByCategory/{id}")
     public ResponseEntity<List<ProductWrapper>> getByCategory(@PathVariable Long id) {
-        try {
             return productService.getByCategory(id);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @GetMapping("/getById/{id}")
     public ResponseEntity<ProductWrapper> getProductId(@PathVariable Long id) {
-        try {
             return productService.getProductId(id);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-        return new ResponseEntity<>(new ProductWrapper(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 }
